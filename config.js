e8c4be1abdc584100913c02b31bf13c41baa1ea8
leelihsun <92011884+leelihsun@users.noreply.github.com>
2026-08-04 09:05:43 +0800

    更新 2026 年 7 月 Google Sheet

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  sheetId: "1Cdaui6RBqBMKHbsisoEifEJ3J7lAvXnu15Q9ymj2FTU",
+  sheetId: "1t0Y1w-yCAffXmjI-cV6SWpDW64lhT8YaUCSyTPdf8WI",
   sheetName: "情報事件",
   rowsPerPage: 20
 };
